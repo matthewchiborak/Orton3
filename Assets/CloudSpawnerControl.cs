eb5294b75e8502c65066a8b87ccb3ed62c689a55
matthewchiborak <matthewchiborak@hotmail.com>
2017-09-03 16:57:43 -0400

    Final level all environments are done. Next step I think that I should do is to modify the stored info script to reflect the modified structure of the game that I want to achieve.

---
+++ Assets/CloudSpawnerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawnerControl : MonoBehaviour {
+
+    public GameObject cloud;
+    public Vector2 xBounds;
+    public Vector2 zBounds;
+    public Vector2 scaleRange;
+    public float spawnHeight;
+    public float timeBetweenClouds;
+
+    private float timeOfLastCloud;
+    private GameObject temp;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Time.time - timeOfLastCloud > timeBetweenClouds)
+        {
+            timeOfLastCloud = Time.time;
+
+            temp = Instantiate(cloud, new Vector3(Random.Range(xBounds.x, xBounds.y), spawnHeight, Random.Range(zBounds.x, zBounds.y)), Quaternion.identity);
+            temp.transform.localScale = new Vector3(Random.Range(scaleRange.x, scaleRange.y), Random.Range(scaleRange.x, scaleRange.y), Random.Range(scaleRange.x, scaleRange.y));
+        }	
+	}
+}
---
+++ Assets/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    public float lifetime;
+    public Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        //When an object with this is created, have it travel in a certain direction until it hits something. 
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = direction * speed;
+
+        Destroy(gameObject, lifetime);
+    }
+}
