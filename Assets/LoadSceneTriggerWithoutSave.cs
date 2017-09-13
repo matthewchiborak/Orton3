632d23a20b47e170c51fdda4bc38a47f63f02cee
matthewchiborak <matthewchiborak@hotmail.com>
2017-09-12 20:47:37 -0400

    Finished setting up level 2s guards and triggers

---
+++ Assets/LoadSceneTriggerWithoutSave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+
+public class LoadSceneTriggerWithoutSave : MonoBehaviour {
+
+    public Image blackScreen;
+    public Text loadingText;
+
+    public string sceneToLoad;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            blackScreen.enabled = true;
+            loadingText.enabled = true;
+
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        }
+    }
+}
