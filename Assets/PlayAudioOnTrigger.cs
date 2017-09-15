f091673b5650d5426b74e3e0f0bbc3556bc0d60b
matthewchiborak <matthewchiborak@hotmail.com>
2017-09-15 16:46:21 -0400

    Finished the river level except for anything cosmettic

---
+++ Assets/PlayAudioOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnTrigger : MonoBehaviour {
+
+    public BoxCollider hitbox;
+    public AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hitbox.enabled = false;
+            audioSource.Play();
+        }
+    }
+}
