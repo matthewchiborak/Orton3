22f9536b5d73ce54c488619daf4e38c1b32b8659
matthewchiborak <matthewchiborak@hotmail.com>
2017-09-17 16:22:10 -0400

    Finished the microwave wallway

---
+++ Assets/HallwayCrawlControl.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayCrawlControl : MonoBehaviour
+{
+    public Transform shawnTransform;
+
+    private bool keyPressed;
+
+    private int timesPressed;
+
+    public int pressesBeforeMove;
+    private bool oneArm;
+
+    public Animator anim;
+
+    private bool moving;
+    public float durationOfMove;
+    private float timeMoveBegan;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public float distanceInEachCrawl;
+
+    private int numberOfPresses2;
+    private int numberOfPresses3;
+
+    private bool exp1;
+    private bool exp2;
+    public ParticleSystem explosion1;
+    public ParticleSystem explosion2;
+
+    public AudioSource explosionSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        numberOfPresses2 = (int)(1.5 * pressesBeforeMove);
+        numberOfPresses3 = 2 * pressesBeforeMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shawnTransform.position.z < -250 && shawnTransform.position.z > -425)
+        {
+            if(!exp1)
+            {
+                exp1 = true;
+                explosion1.Play();
+                explosionSource.Play();
+            }
+
+            pressesBeforeMove = numberOfPresses2;
+        }
+        else if(shawnTransform.position.z < -425)
+        {
+            if (!exp2)
+            {
+                exp2 = true;
+                explosion2.Play();
+                explosionSource.Play();
+            }
+
+            pressesBeforeMove = numberOfPresses3;
+        }
+
+        if(moving)
+        {
+            shawnTransform.position = Vector3.Lerp(startPos, endPos, (Time.time - timeMoveBegan) / durationOfMove);
+            if ((Time.time - timeMoveBegan) > durationOfMove)
+            {
+                moving = false;
+            }
+        }
+
+        if(timesPressed > pressesBeforeMove && !moving)
+        {
+            if(oneArm)
+            {
+                oneArm = false;
+                timesPressed = 0;
+                anim.Play("Armature|Crawl1", -1, 0);
+                moving = true;
+                startPos = shawnTransform.position;
+                timeMoveBegan = Time.time;
+                endPos = new Vector3(startPos.x, startPos.y, startPos.z - distanceInEachCrawl);
+            }
+            else
+            {
+                oneArm = true;
+                timesPressed = 0;
+                anim.Play("Armature|Crawl2", -1, 0);
+                moving = true;
+                startPos = shawnTransform.position;
+                timeMoveBegan = Time.time;
+                endPos = new Vector3(startPos.x, startPos.y, startPos.z - distanceInEachCrawl);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        if(!keyPressed && moveVertical > 0)
+        {
+            keyPressed = true;
+            timesPressed++;
+        }
+
+        if (keyPressed && moveVertical == 0)
+        {
+            keyPressed = false;
+        }
+    }
+}
---
+++ Assets/HallwayLoadSceneTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+
+public class HallwayLoadSceneTrigger : MonoBehaviour {
+
+    public Image blackScreen;
+    public Text loadingText;
+
+    private List<string> parsedLine;
+
+    public LevelID sceneToLoad;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            string assetText;
+            parsedLine = new List<string>();
+
+            using (var streamReader = new StreamReader("Assets/Resources/Save.txt", Encoding.UTF8))
+            {
+                assetText = streamReader.ReadToEnd();
+            }
+
+            //Parse
+            parsedLine.AddRange(assetText.Split("~"[0]));
+            
+            blackScreen.enabled = true;
+            loadingText.enabled = true;
+            
+            string data;
+
+            
+            data = ((int)sceneToLoad).ToString() + "~0~" + parsedLine[2] + "~" + parsedLine[3] + "~" + parsedLine[4] + "~" + parsedLine[5] + "~" + parsedLine[6] + "~" + parsedLine[7] + "~" + parsedLine[8];
+            
+            System.IO.File.WriteAllText("Assets/Resources/Save.txt", data);
+
+            SceneManager.LoadScene(((SceneNames)((int)sceneToLoad)).ToString(), LoadSceneMode.Single);
+        }
+    }
+}
